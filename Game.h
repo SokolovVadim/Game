e81bbd3015fbd78a524479afe36ecb5b9db10058
Vadim Sokolov <varem99@mail.ru>
2018-04-12 10:36:50 +0300

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+#include <vector>
+
+void DrawTexture(float x, float y);
+void DrawFunc();
+void UseThread();
+void DrawWindow();
+void DrawPict();
+
+
+enum POSITIONS
+{
+	XCOORD = 100,
+	YCOORD = 100
+};
