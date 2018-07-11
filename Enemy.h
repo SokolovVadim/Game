d32af6ade812ae11ba79db0acf5c3728caefe019
Vadim Sokolov <varem99@mail.ru>
2018-07-11 18:35:13 +0300

    Add files via upload

--- Enemy.h
+++ Enemy.h
@@ -21,10 +21,36 @@ Enemy::Enemy(sf::Image & image_, std::string name_, float x, float y, float w, f
 	Entity(image_, name_, x, y, w, h)
 {
 	if (name_ == "Archer1") {
-		dx = enemy_speed;
+		dx = -enemy_speed;
 		sprite.setTextureRect(sf::IntRect(LEFT, TOP, W, H));
 	}
 }
+
+
+
+//
+//f(dy_ > 0)
+//{
+//	Ycoord = i * HGRASS - Height + 15.9999f;
+//	dy = 0;
+//	OnGround = true;
+//}
+//if (dy_ < 0)
+//{
+//	Ycoord = i * HGRASS + Height / 2 + 18;
+//	dy = 0;
+//}
+//if (dx_ > 0)
+//{
+//	Xcoord = j * WGRASS - Width + 47.99f;
+//	dx = 0;
+//}
+//if (dx_ < 0)
+//{
+//	Xcoord = j * WGRASS + Width / 2 + 1;
+//	dx = 0;
+//}
+
 
 void Enemy::CheckCollision(Map & map, float dx_, float dy_)
 {
@@ -46,11 +72,12 @@ void Enemy::CheckCollision(Map & map, float dx_, float dy_)
 				{
 					Xcoord = j * WGRASS - Width + 47.99f;
 					dx = -enemy_speed;
-					sprite.setScale(-1, 1);
+					sprite.setScale(1, 1);
+					//break;
 				}
 				if (dx_ < 0)
 				{
-					Xcoord = j * WGRASS + WGRASS ;
+					Xcoord = float(j * WGRASS + WGRASS);
 					dx = enemy_speed;
 					sprite.setScale(-1, 1);
 				}
@@ -62,11 +89,11 @@ void Enemy::Update(Map & map, sf::Int64 time)
 	if (Name == "Archer1")
 	{
 		Timer += time;
-		if (Timer >= 3000.0f)
+		/*if (Timer >= 3000.0f)
 		{
 			dx *= -1;
 			Timer = 0;
-		}
+		}*/
 		CheckCollision(map, dx, 0);
 		Xcoord += dx * time;
 		sprite.setPosition(Xcoord + W / 2, Ycoord + H / 2);
