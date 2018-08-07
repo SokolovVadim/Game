f4d26ee5b9900713d73b6340bbd69569d4f74427
Vadim Sokolov <varem99@mail.ru>
2018-08-07 22:18:31 +0300

    Add files via upload

---
+++ Bullet.h
@@ -0,0 +1,67 @@
+#pragma once
+
+
+
+namespace bulletStuff {
+
+
+
+	class Bullet: public Entity
+	{
+	public:
+
+		Bullet(const std::string file, const std::string name_, float x, float y, float w, float h, int dir);
+
+		void Update(const float time);
+
+	private:
+		
+		int direction;
+		const float bulletSpeed = 0.9f;
+	};
+
+
+	Bullet::Bullet(const std::string file, const std::string name_, float x, float y, float w, float h, int dir) :
+		Entity			(file, name_, x, y, w, h),
+		direction		(dir)
+	{
+		Speed = bulletSpeed;
+	}
+
+
+	void Bullet::Update(const float time)
+	{
+		switch (direction)
+		{
+		case 0: {
+			dx = -Speed, dy = 0;
+			break;
+		}
+		case 1: {
+			dx = Speed, dy = 0;
+			break;
+		}
+		case 2: {
+			dx = 0, dy = -Speed;
+			break;
+		}
+		case 3: {
+			dx = 0, dy = Speed;
+			break;
+		}
+		case 4: {
+			dx = Speed, dy = -Speed;
+			//dx = 0, dy = 0;
+		}
+		default: {
+			dx = 0; dy = 0;
+		}
+		};
+	}
+
+	/*class EnemyBullet :public Bullet
+	{
+
+	};*/
+
+}
