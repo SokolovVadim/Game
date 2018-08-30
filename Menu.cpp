2e58ac2268c509252580b915374c67d864dc02fa
Vadim Sokolov <varem99@mail.ru>
2018-08-30 22:20:13 +0300

    Add files via upload

--- Menu.cpp
+++ Menu.cpp
@@ -1,35 +1,64 @@
 #include "Menu.h"
 namespace menu {
 
-	Menu::Menu() :
-		_file					(""),
-		_texture				(sf::Texture()),
-		_sprite					(_texture),
-		_isShow					(true),
-		_bannerNumber			(BANNER::_PLAY)
+
+	MenuSet::MenuSet() :
+		_file(""),
+		_texture(sf::Texture()),
+		_sprite(_texture)
+	{}
+
+	MenuSet::MenuSet(const std::string & file) :
+		_file(file),
+		_texture(sf::Texture()),
+		_sprite(sf::Sprite())
 	{
 		_texture.loadFromFile("Images/" + _file);
+		_sprite.setTexture(_texture);
 	}
 
-	Menu::Menu(const std::string & file) :
-		_file					(file),
-		_texture				(sf::Texture()),
-		_sprite					(_texture),
+	MenuSet::~MenuSet()
+	{}
+
+	Menu::Menu() :
+		_numbSets				(0ul),
+		_setsVec				(_numbSets),
 		_isShow					(true),
 		_bannerNumber			(BANNER::_PLAY)
 	{
-		_texture.loadFromFile("Images/" + _file);
+		//_texture.loadFromFile("Images/" + _file);
+	}
+
+	Menu::Menu(std::size_t size, ...) :
+		_numbSets				(size),
+		_setsVec				(_numbSets),
+		_isShow					(true),
+		_bannerNumber			(BANNER::_PLAY)
+	{
+		va_list args;
+
+		va_start(args, size);
+
+		while (size--)
+		{
+			_setsVec.push_back(*(new MenuSet(va_arg(args, const std::string))));
+		}
+
+		va_end(args);
+
+		/*_texture.loadFromFile("Images/" + _file);
+		_sprite.setTexture(_texture);*/
 	}
 
 	Menu::~Menu()
 	{}
 
-	void Menu::setPosition(const sf::Vector2f & pos)
+	void MenuSet::setPosition(const sf::Vector2f & pos)
 	{
 		_sprite.setPosition(pos);
 	}
 
-	void Menu::draw(sf::RenderWindow & window)
+	void MenuSet::draw(sf::RenderWindow & window)
 	{
 		window.draw			(_sprite);
 		window.display		();
@@ -40,7 +69,7 @@ namespace menu {
 		return _isShow;
 	}
 
-	void Menu::setColor(const sf::Color color)
+	void MenuSet::setColor(const sf::Color color)
 	{
 		_sprite.setColor(color);
 	}
@@ -72,51 +101,49 @@ namespace menu {
 
 	void showMenu(sf::RenderWindow & window, const std::string & file)
 	{
-		Menu menu(file);
-		menu.setPosition(sf::Vector2f(100.f, 100.f)); // it is not so bad
+		Menu menu(2, "Menu1.jpg", "play.png");
+		//menu.setPosition(sf::Vector2f(0.f, 0.f)); // it is not so bad
 													  // but it should be corrected
 		while (menu.isDrawable())
 		{
-			menu.setColor				(sf::Color::White);
+			//menu.setColor				(sf::Color::White);
 			menu.setMenuNum				(0);
-			//window.clear(sf::Color(10, 10, 10, 10));
+			window.clear(sf::Color(10, 10, 10, 10));
 
-			if (sf::IntRect(100, 100, 100, 100).contains(sf::Mouse::getPosition(window))) // play
-			{
-				menu.setColor					(sf::Color::Blue);
-				menu.setMenuNum					(0);
-			}
-			if (sf::IntRect(100, 200, 100, 100).contains(sf::Mouse::getPosition(window))) // exit
-			{
-				menu.setColor					(sf::Color::Blue);
-				menu.setMenuNum					(1);
-			}
-			if (sf::IntRect(100, 300, 100, 100).contains(sf::Mouse::getPosition(window))) // smth new (sttngs?)
-			{
-				menu.setColor					(sf::Color::Blue);
-				menu.setMenuNum					(2);
-			}
+			//if (sf::IntRect(100, 100, 100, 100).contains(sf::Mouse::getPosition(window))) // play
+			//{
+			//	menu.setColor					(sf::Color::Blue);
+			//	menu.setMenuNum					(0);
+			//}
+			//if (sf::IntRect(100, 200, 100, 100).contains(sf::Mouse::getPosition(window))) // exit
+			//{
+			//	menu.setColor					(sf::Color::Blue);
+			//	menu.setMenuNum					(1);
+			//}
+			//if (sf::IntRect(100, 300, 100, 100).contains(sf::Mouse::getPosition(window))) // smth new (sttngs?)
+			//{
+			//	menu.setColor					(sf::Color::Blue);
+			//	menu.setMenuNum					(2);
+			//}
 
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) // play
-			{
-				int status = menu.getStatus();
+			//if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) // play
+			//{
+			//	int status = menu.getStatus();
 
-				switch (status)
-				{
-				case 0:
-					menu.setMenuStatus(false);
-				case 1:
-				{
-					window.close();
-					menu.setMenuStatus(false);
-				}
-				}
+			//	switch (status)
+			//	{
+			//	case 0:
+			//		menu.setMenuStatus(false);
+			//	case 1:
+			//	{
+			//		window.close();
+			//		menu.setMenuStatus(false);
+			//	}
+			//	}
 
-			}
+			//}
 
-			menu.draw(window);
-
-
+			//menu.draw(window);
 		}
 	}
 
--- Menu.h
+++ Menu.h
@@ -4,12 +4,33 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <stdarg.h>
 
 //-----------------------------------------------------------------------
 /// \brief Menu class to choose the game mode or settings
 //-----------------------------------------------------------------------
 
+
+
 namespace menu {
+
+	class MenuSet
+	{
+	public:
+		MenuSet								();
+		MenuSet								(const std::string & file);
+		~MenuSet							();
+		void		draw					(sf::RenderWindow & window);
+		void		setPosition				(const sf::Vector2f & pos);
+		void		setColor				(const sf::Color color);
+
+	private:
+		std::string			_file;
+		sf::Texture			_texture;
+		sf::Sprite			_sprite;
+	};
+
+
 	class Menu
 	{
 
@@ -21,14 +42,14 @@ namespace menu {
 		};
 	public:
 		Menu();
-		Menu(const std::string & file);
+		Menu(std::size_t size, ...);
 		~Menu();
 
-		void			setPosition	(const sf::Vector2f & pos);
+		/*void			setPosition	(const sf::Vector2f & pos);
 		void			draw(sf::RenderWindow & window);
-		void			setColor(const sf::Color color);
+		void			setColor(const sf::Color color);*/
 		void			setMenuNum(int number);
-		void setMenuStatus(const bool status);
+		void			setMenuStatus(const bool status);
 
 		const bool isDrawable() const;
 		const BANNER getStatus() const;
@@ -38,9 +59,13 @@ namespace menu {
 
 
 	private:
-		std::string			_file;
-		sf::Texture			_texture;
-		sf::Sprite			_sprite;
+
+		std::size_t				_numbSets;
+		std::vector<MenuSet>	_setsVec;
+
+		/*std::string				_file;
+		sf::Texture				_texture;
+		sf::Sprite				_sprite;*/
 
 		//sf::RenderWindow	_window;
 
